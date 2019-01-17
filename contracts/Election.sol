f5152b4364a7e1362fc2d192ccba8216a14f7d6f
Sneh <snehkoul1999@gmail.com>
2019-01-17 12:09:23 -0500

    deployed sample contract

---
+++ contracts/Election.sol
@@ -0,0 +1,9 @@
+pragma solidity ^0.5.0;
+
+contract Election {
+    //Store Candidate
+    //Read Candidate
+    //Constructor
+    string public candidate;
+    constructor() public { candidate = "Candidate 1";}      
+    }
